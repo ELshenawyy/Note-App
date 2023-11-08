7d9c64c31b1358b8a87d07a95c23a87d810f0595
ELshenawyy <alshnawyahmd668@gmail.com>
2023-11-08 18:12:52 +0200

    create notes list view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -8,7 +8,7 @@ class NoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body:NotesViewBody() ,
+      body:NotesViewBody(),
     );
   }
 }
--- lib/views/notes_view_body.dart
+++ lib/views/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
-import 'custom_note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,7 +15,9 @@ class NotesViewBody extends StatelessWidget {
             height: 30,
           ),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(
+            child: ListViewItem(),
+          ),
         ],
       ),
     );
