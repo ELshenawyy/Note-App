9fbb28da75c9866e18cfea3e8f097dce6f57c4da
ELshenawyy <alshnawyahmd668@gmail.com>
2023-11-09 00:27:25 +0200

    create button sheet

--- lib/views/add_note_bottom_sheet.dart
+++ lib/views/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:note_app/views/custom_text_field.dart';
+
+import 'button_sheet.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -8,22 +10,31 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          AddTextField(
-            hint: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          AddTextField(
-            hint: 'content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            AddTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            AddTextField(
+              hint: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
